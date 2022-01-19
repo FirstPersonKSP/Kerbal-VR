98e2a744fa20e9269768ec02efd59bca8df1b699
Jon Cable <jonnyothan@gmail.com>
2022-01-18 21:03:06 -0500

    beginnings for interactive switch prop

---
+++ KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_Switch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KerbalVR.InternalModules
+{
+	class VRSwitch : InternalModule
+	{
+		[KSPField]
+		public string switchTransformName = null;
+
+		[KSPField]
+		public Vector3 rotationAxis = Vector3.right;
+
+		[KSPField]
+		public float minAngle = -30;
+
+		[KSPField]
+		public float maxAngle = 30;
+
+#if PROP_GIZMOS
+		GameObject gizmo;
+#endif
+	}
+}
