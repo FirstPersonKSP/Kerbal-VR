da24dc8493c1b9cb8675eaa0ff43359b476a36a2
JonnyOThan <jonnyothan@gmail.com>
2022-06-17 16:43:45 -0400

    mk2InlineCockpit issues -fix seat prop transforms -use stock model and add new InternalObjectDeleter to remove the builtin seats

---
+++ KerbalVR_Mod/KerbalVR/InternalModules/InternalObjectDeleter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KerbalVR.InternalModules
+{
+	class InternalObjectDeleter : InternalModule
+	{
+		public string[] m_objectNames;
+
+		public override void OnLoad(ConfigNode node)
+		{
+			base.OnLoad(node);
+			m_objectNames = node.GetValues("objectName");
+		}
+
+		private void Start()
+		{
+			foreach (var name in m_objectNames)
+			{
+				var transform = this.FindTransform(name);
+
+				if (transform != null)
+				{
+					Destroy(transform.gameObject);
+				}
+			}
+		}
+	}
+}
