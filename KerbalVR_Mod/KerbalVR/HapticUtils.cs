dc387c57696f774fa6679f8d9068578abd2e7f18
Icecovery <Icecovery@foxmail.com>
2022-09-06 18:16:21 -0400

    Added haptic settings

--- KerbalVR_Mod/KerbalVR/HapticUtils.cs
+++ KerbalVR_Mod/KerbalVR/HapticUtils.cs
@@ -1,34 +1,74 @@
-﻿using System;
+﻿using KSPDev.ConfigUtils;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 using Valve.VR;
 
 namespace KerbalVR
 {
-	public static class HapticUtils
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	[PersistentFieldsDatabase("KerbalVR/settings/KerbalVRConfig")]
+	public class HapticUtils : MonoBehaviour
 	{
+		public class HapticLevel
+		{
+			[PersistentField("name")]
+			public string name;
+			[PersistentField("duration")]
+			public float duration;
+			[PersistentField("frequency")]
+			public float frequncy;
+			[PersistentField("amplitude")]
+			public float amplitude;
+		}
+
+		[PersistentField("Haptic/enabled")]
+		public bool hapticEnabledConfig = true;
+
+		[PersistentField("Haptic/Level", isCollection = true)]
+		public readonly List<HapticLevel> hapticLevelsConfig = new List<HapticLevel>();
+
+		private static bool hapticEnabled;
+		private static Dictionary<string, HapticLevel> hapticLevels;
 		private readonly static SteamVR_Action_Vibration hapticAction = SteamVR_Actions.default_Haptic;
+
+		private void Awake()
+		{
+			ConfigAccessor.ReadFieldsInType(GetType(), this);
+
+			hapticEnabled = hapticEnabledConfig;
+			hapticLevels = hapticLevelsConfig.ToDictionary(x => x.name);
+		}
 
 		public static void Light(SteamVR_Input_Sources inputSource)
 		{
-			Pulse(0.005f, 0.005f, 1f, inputSource);
+			Pulse(hapticLevels["Light"], inputSource);
 		}
 
 		public static void Heavy(SteamVR_Input_Sources inputSource)
 		{
-			Pulse(0.1f, 100f, 1f, inputSource);
+			Pulse(hapticLevels["Heavy"], inputSource);
 		}
 
 		public static void Snap(SteamVR_Input_Sources inputSource)
 		{
-			Pulse(0.03f, 200f, 1f, inputSource);
+			Pulse(hapticLevels["Snap"], inputSource);
+		}
+
+		public static void Pulse(HapticLevel hapticSetting, SteamVR_Input_Sources inputSource)
+		{
+			if (hapticEnabled)
+			{
+				hapticAction.Execute(0f, hapticSetting.duration, hapticSetting.frequncy, hapticSetting.amplitude, inputSource);
+			}
 		}
 
 		public static void Pulse(float duration, float frequncy, float amplitude, SteamVR_Input_Sources inputSource)
 		{
-			hapticAction.Execute(0f, duration, frequncy, amplitude, inputSource);
+			if (hapticEnabled)
+			{
+				hapticAction.Execute(0f, duration, frequncy, amplitude, inputSource);
+			}	
 		}
 	}
 }
