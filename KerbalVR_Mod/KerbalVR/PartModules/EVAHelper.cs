a0a9a9746ab6e8e8b7336023980c5f6fc837ac8b
JonnyOThan <jonnyothan@gmail.com>
2022-09-26 16:34:24 -0400

    Fix #156: Create a PartModule for toggling eva lamp/jetpack in the PAW

---
+++ KerbalVR_Mod/KerbalVR/PartModules/EVAHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KerbalVR.PartModules
+{
+	public class VREVAHelper : PartModule
+	{
+		KerbalEVA m_eva;
+
+		void Start()
+		{
+			m_eva = GetComponent<KerbalEVA>();
+			LampChanged();
+			JetpackChanged();
+		}
+
+		[KSPEvent(guiActive = true)]
+		void ToggleLamp()
+		{
+			m_eva.ToggleLamp();
+			LampChanged();
+		}
+
+		void LampChanged()
+		{
+			// really this should be a postfix or something..
+			base.Events["ToggleLamp"].guiName = m_eva.lampOn ? "Deactivate Lamp" : "Activate Lamp";
+		}
+
+		[KSPEvent(guiActive = true)]
+		void ToggleJetpack()
+		{
+			m_eva.ToggleJetpack();
+			JetpackChanged();
+		}
+
+		void JetpackChanged()
+		{
+			base.Events["ToggleJetpack"].guiName = m_eva.JetpackDeployed ? "Deactivate Jetpack" : "Activate Jetpack";
+		}
+	}
+}
