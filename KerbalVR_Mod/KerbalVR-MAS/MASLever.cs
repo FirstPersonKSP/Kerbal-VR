876bebfb58b8c1b249a6a33a0825cab1e6124093
Icecovery <Icecovery@foxmail.com>
2022-10-07 20:09:56 -0400

    Added MAS levers

---
+++ KerbalVR_Mod/KerbalVR-MAS/MASLever.cs
@@ -0,0 +1,156 @@
+﻿using AvionicsSystems;
+using KerbalVR;
+using KerbalVR.InternalModules;
+using KerbalVR.IVAAdaptors;
+using System;
+
+namespace KerbalVR_MAS
+{
+	public class MASLever : IVALever
+	{
+		public static MASLever TryConstruct(VRLever vrLever)
+		{
+			MASComponent component = vrLever.GetComponent<MASComponent>(); 
+
+			if (component)
+			{
+				return new MASLever(component, vrLever);
+			}
+
+			return null;
+		}
+
+		VRLever lever;
+		MASComponentAnimation componentAnimation;
+		MASComponentAnimationPlayer componentAnimationPlayer;
+		MASComponentRotation componentRotation;
+		MASFlightComputer flightComputer;
+
+		// cache it because some MAS functions won't update value instantly
+		int lastStep;
+
+		public MASLever(MASComponent component, VRLever vrLever)
+		{
+			lever = vrLever;
+
+			foreach (IMASSubComponent action in component.actions)
+			{
+				if (action is MASComponentAnimation componentAnimation)
+				{
+					this.componentAnimation = componentAnimation;
+				}
+				else if (action is MASComponentAnimationPlayer componentAnimationPlayer)
+				{
+					this.componentAnimationPlayer = componentAnimationPlayer;
+				}
+				else if (action is MASComponentRotation componentRotation)
+				{
+					this.componentRotation = componentRotation;
+				}
+			}
+
+			flightComputer = vrLever.part.GetComponent<MASFlightComputer>();
+
+			lastStep = RefreshState();
+		}
+
+		public override void SetStep(int stepId)
+		{
+			switch (lever.handler)
+			{
+				case "Gear":
+					flightComputer.fcProxy.SetGear(stepId == 1);
+					break;
+				case "Spoiler":
+					flightComputer.farProxy.SetSpoilers(stepId == 1);
+					break;
+				case "Flap":
+					int currentSetting = (int)Math.Round(flightComputer.farProxy.GetFlapSetting());
+					if (currentSetting >= 0)
+					{
+						int delta = stepId - currentSetting;
+						if (delta < 0)
+						{
+							for (int i = 0; i > delta; --i)
+							{
+								flightComputer.farProxy.DecreaseFlapSetting();
+							}
+						}
+						else if (delta > 0)
+						{
+							for (int i = 0; i < delta; ++i)
+							{
+								flightComputer.farProxy.IncreaseFlapSetting();
+							}
+						}
+					}
+					break;
+				default:
+					Utils.LogWarning($"Unknown lever handler {lever.handler}");
+					break;
+			}
+
+			lastStep = stepId;
+
+			// Manually set correct state for these modules so they won't play
+			if (componentAnimation != null)
+			{
+				componentAnimation.currentBlend = stepId;
+				componentAnimation.animationState.time = stepId * componentAnimation.animationState.length;
+			}
+			if (componentAnimationPlayer != null)
+			{
+				componentAnimationPlayer.currentState = stepId == 1;
+			}
+			if (componentRotation != null)
+			{
+				componentRotation.currentBlend = (float)stepId / (lever.stepCount - 1);
+			}
+		}
+
+		public override int GetStep()
+		{
+			return lastStep;
+		}
+
+		public int RefreshState()
+		{
+			switch (lever.handler)
+			{
+				case "Gear":
+					Utils.Log($"Gear {flightComputer.fcProxy.GetGear()}");
+					return (int)Math.Round(flightComputer.fcProxy.GetGear());
+				case "Spoiler":
+					Utils.Log($"Spoiler {flightComputer.farProxy.GetSpoilerSetting()}");
+					return (int)Math.Round(flightComputer.farProxy.GetSpoilerSetting());
+				case "Flap":
+					Utils.Log($"Flap {flightComputer.farProxy.GetFlapSetting()}");
+					return (int)Math.Round(flightComputer.farProxy.GetFlapSetting());
+				default:
+					Utils.LogWarning($"Unknown lever handler {lever.handler}");
+					break;
+			}
+
+			return 0;
+		}
+
+		public override void SetUpdateEnabled(bool enabled)
+		{
+			if (componentAnimation != null && componentAnimation.variableRegistrar.isEnabled != enabled)
+			{
+				componentAnimation.variableRegistrar.EnableCallbacks(enabled);
+				componentAnimation.animationState.enabled = enabled;
+			}
+			if (componentAnimationPlayer != null && componentAnimationPlayer.variableRegistrar.isEnabled != enabled)
+			{
+				componentAnimationPlayer.variableRegistrar.EnableCallbacks(enabled);
+			}
+			if (componentRotation != null && componentRotation.variableRegistrar.isEnabled != enabled)
+			{
+				componentRotation.variableRegistrar.EnableCallbacks(enabled);
+			}
+
+			lastStep = RefreshState();
+		}
+	}
+}
