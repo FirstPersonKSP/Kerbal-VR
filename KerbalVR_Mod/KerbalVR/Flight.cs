fd71b023915f00475b1cb7527ab68d1cf3485de1
JonnyOThan <jonnyothan@gmail.com>
2022-10-13 06:49:13 -0400

    Re-enable keyboard controls for eva jump and sprint

--- KerbalVR_Mod/KerbalVR/Flight.cs
+++ KerbalVR_Mod/KerbalVR/Flight.cs
@@ -295,11 +295,10 @@ namespace KerbalVR
 				FirstPerson.FirstPersonEVA.instance.fpStateWalkRun.evt_OnEnterFirstPerson(kerbalEVA);
 				FirstPerson.FirstPersonEVA.instance.fpStateFloating.evt_OnEnterFirstPerson(kerbalEVA);
 
-				kerbalEVA.On_jump_start.OnCheckCondition = (KFSMState currentState) => kerbalEVA.VesselUnderControl && m_jumpAction.state && !kerbalEVA.PartPlacementMode && !EVAConstructionModeController.MovementRestricted;
+				kerbalEVA.On_jump_start.OnCheckCondition = (KFSMState currentState) => kerbalEVA.VesselUnderControl && (m_jumpAction.state || GameSettings.EVA_Jump.GetKeyDown()) && !kerbalEVA.PartPlacementMode && !EVAConstructionModeController.MovementRestricted;
 
-
-				kerbalEVA.On_startRun.OnCheckCondition = (KFSMState currentState) => kerbalEVA.VesselUnderControl && m_isSprinting;
-				kerbalEVA.On_endRun.OnCheckCondition = (KFSMState currentState) => kerbalEVA.VesselUnderControl && !m_isSprinting;
+				kerbalEVA.On_startRun.OnCheckCondition = (KFSMState currentState) => kerbalEVA.VesselUnderControl && (m_isSprinting || GameSettings.EVA_Run.GetKeyDown());
+				kerbalEVA.On_endRun.OnCheckCondition = (KFSMState currentState) => kerbalEVA.VesselUnderControl && !m_isSprinting && !GameSettings.EVA_Run.GetKeyDown();
 
 				JetpackPrecisionMode = true;
 				m_isSprinting = false;
