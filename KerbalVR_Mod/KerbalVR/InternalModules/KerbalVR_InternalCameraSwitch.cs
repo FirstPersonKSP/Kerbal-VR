a84496f2f286979a651bfaf0fd1ffde03ac25b43
JonnyOThan <jonnyothan@gmail.com>
2022-10-17 20:22:04 -0400

    PCR: Support swapping seats

---
+++ KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_InternalCameraSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KerbalVR.InternalModules
+{
+	internal class VRInternalCameraSwitch : InternalCameraSwitch
+	{
+		InteractableBehaviour interactableBehaviour;
+
+		public override void OnAwake()
+		{
+			base.OnAwake();
+
+			if (colliderTransform != null)
+			{
+				interactableBehaviour = Utils.GetOrAddComponent<InteractableBehaviour>(colliderTransform.gameObject);
+
+				interactableBehaviour.OnGrab = OnGrab;
+
+				// TODO: do we need to figure out how to disable the collider on the current camera point?
+			}
+		}
+
+		private void OnGrab(Hand hand)
+		{
+			Button_OnDoubleTap();
+		}
+	}
+}
