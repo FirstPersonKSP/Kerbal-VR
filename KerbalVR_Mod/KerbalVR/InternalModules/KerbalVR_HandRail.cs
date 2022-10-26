cb3646da9608140de0b0178c729203bf1780272c
JonnyOThan <jonnyothan@gmail.com>
2022-10-25 21:59:29 -0400

    remove ASET-based hand rail props hook up VRHandRail module to freeIVA set up patches for ASET props

--- KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_HandRail.cs
+++ KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_HandRail.cs
@@ -1,62 +1,40 @@
+using System;
 using UnityEngine;
 using Valve.VR;
-#if false
 
-namespace KerbalVR.InternalModules {
-	public class HandRail : InteractableInternalModule {
-
-#region KSP Config Fields
+namespace KerbalVR.InternalModules
+{
+	
+	// this will probably eventually be moved to Common so that we can have rails on the exterior too
+	public class VRHandRail : InternalModule
+	{
 		[KSPField]
-		public float railLength = 0.1f;
-#endregion
+		public string railTransformName = string.Empty;
 
-#region Private Members
-		protected ConfigNode moduleConfigNode;
-		protected CapsuleCollider handleCollider;
-#endregion
+		InteractableBehaviour m_interactable;
 
-		protected void Start() {
-			// no setup needed in editor mode
-			if (HighLogic.LoadedScene == GameScenes.EDITOR) return;
 
-			// obtain module configuration
-			moduleConfigNode = KerbalVR.ConfigUtils.GetModuleConfigNode(internalProp.name, moduleID);
-
-			// add an interactable collider
-			handleCollider = this.gameObject.AddComponent<CapsuleCollider>();
-			handleCollider.isTrigger = true;
-			handleCollider.center = new Vector3(0f, 0f, -0.0527f);
-			handleCollider.radius = 0.01f;
-			handleCollider.height = railLength;
-			handleCollider.direction = 1; // y-axis aligned
-
-			// add a pose for this object
-			SkeletonPoser = this.gameObject.AddComponent<SteamVR_Skeleton_Poser>();
-			SkeletonPoser.skeletonMainPose = SkeletonPose_HandleRailGrabPose.GetInstance();
-			SkeletonPoser.Initialize();
-
-			/*
-			GameObject gizmo = Utils.CreateGizmo(0.25f);
-			gizmo.transform.SetParent(this.transform);
-			gizmo.transform.localPosition = Vector3.zero;
-			gizmo.transform.localRotation = Quaternion.identity;
-			gizmo.transform.localScale = Vector3.one;
-
-			this.gameObject.AddComponent<ColliderVisualizer>();
-			*/
-
-			// set layer for this object to 20 (Internal Space)
-			Utils.SetLayer(this.gameObject, 20);
+		void Start()
+		{
+			var transform = this.FindTransform(railTransformName);
+			if (transform != null)
+			{
+				var colliderTransform = transform.GetComponentInChildren<Collider>();
+				if (colliderTransform != null)
+				{
+					m_interactable = Utils.GetOrAddComponent<InteractableBehaviour>(colliderTransform.gameObject);
+					m_interactable.OnGrab += Grabbed;
+				}
+				else
+				{
+					Utils.LogError($"VRHandRail: No collider under transform ${railTransformName} found for prop {internalProp.propName}");
+				}
+			}
 		}
 
-		protected void Update() {
-			if (IsGrabbed) {
-				Vector3 deltaPos = GrabbedHand.handActionPose.GetLastLocalPosition(GrabbedHand.handType) -
-					GrabbedHand.handActionPose.GetLocalPosition(GrabbedHand.handType);
-				KerbalVR.Scene.Instance.CurrentPosition += KerbalVR.Scene.Instance.CurrentRotation * deltaPos;
-			}
+		private void Grabbed(Hand hand)
+		{
+			FreeIva.KerbalIvaController.Instance.Unbuckle();
 		}
 	}
 }
-
-#endif
