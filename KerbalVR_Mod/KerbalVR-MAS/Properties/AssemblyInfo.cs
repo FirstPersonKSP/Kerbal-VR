1f7301664e1b7cf5f9b291530c084d115e8834c7
JonnyOThan <jonnyothan@gmail.com>
2023-02-02 00:23:58 -0500

    bump version number to 9.0.2

--- KerbalVR_Mod/KerbalVR-MAS/Properties/AssemblyInfo.cs
+++ KerbalVR_Mod/KerbalVR-MAS/Properties/AssemblyInfo.cs
@@ -32,10 +32,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.1")]
-[assembly: AssemblyFileVersion("0.8.1")]
+[assembly: AssemblyVersion("0.9.0.2")]
+[assembly: AssemblyFileVersion("0.9.0.2")]
 
 
-[assembly: KSPAssembly("KerbalVR-MAS", 0, 8, 1)]
+[assembly: KSPAssembly("KerbalVR-MAS", 0, 9, 0)]
 
-// TODO: declare dependency on MAS
+[assembly: KSPAssemblyDependency("MOARdVAvionicsSystems", 1, 3)] 
