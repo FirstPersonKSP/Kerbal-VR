f91213f4a496e9c34395e1d1b6b91452ae966c60
JonnyOThan <jonnyothan@gmail.com>
2023-02-02 16:47:23 -0500

    First pass at grab & pull movement

--- KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_HandRail.cs
+++ KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_HandRail.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using Valve.VR;
 
@@ -13,6 +14,9 @@ namespace KerbalVR.InternalModules
 
 		InteractableBehaviour m_interactable;
 
+		Vector3 m_grabbedPositon;
+		Quaternion m_grabbedRotation;
+		Vector3 velocity;
 
 		void Start()
 		{
@@ -24,17 +28,54 @@ namespace KerbalVR.InternalModules
 				{
 					m_interactable = Utils.GetOrAddComponent<InteractableBehaviour>(colliderTransform.gameObject);
 					m_interactable.OnGrab += Grabbed;
+					m_interactable.OnRelease += Released;
 				}
 				else
 				{
 					Utils.LogError($"VRHandRail: No collider under transform ${railTransformName} found for prop {internalProp.propName}");
 				}
 			}
+
+			enabled = false;
+		}
+
+		private void Released(Hand hand)
+		{
+			FreeIva.KerbalIvaAddon.KerbalIva.FreezeUpdates = false;
+			FreeIva.KerbalIvaAddon.KerbalIva.KerbalRigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+			FreeIva.KerbalIvaAddon.KerbalIva.KerbalRigidbody.velocity = velocity;
+			FreeIva.KerbalIvaAddon.KerbalIva.KerbalRigidbody.WakeUp();
+			velocity = Vector3.zero;
+			enabled = false;
 		}
 
 		private void Grabbed(Hand hand)
 		{
 			FreeIva.KerbalIvaAddon.Instance.Unbuckle();
+
+			m_grabbedPositon = hand.GripPosition;
+			m_grabbedRotation = hand.transform.rotation;
+
+			if (!FreeIva.KerbalIvaAddon.KerbalIva.FreezeUpdates)
+			{
+				enabled = true;
+				FreeIva.KerbalIvaAddon.KerbalIva.FreezeUpdates = true;
+				FreeIva.KerbalIvaAddon.KerbalIva.KerbalRigidbody.interpolation = RigidbodyInterpolation.None;
+			}
+		}
+
+		static float x_gain = 10f;
+
+		void FixedUpdate()
+		{
+			if (m_interactable.IsGrabbed)
+			{
+				Vector3 offset = m_interactable.GrabbedHand.GripPosition - m_grabbedPositon;
+				var rigidBody = FreeIva.KerbalIvaAddon.KerbalIva.KerbalRigidbody;
+				rigidBody.MovePosition(rigidBody.position - offset);
+
+				velocity = (x_gain * -offset / Time.fixedDeltaTime + velocity) / (1 + x_gain);
+			}
 		}
 	}
 }
