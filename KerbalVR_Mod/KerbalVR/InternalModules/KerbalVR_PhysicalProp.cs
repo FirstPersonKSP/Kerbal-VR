d7ce9b62e8d1de0bc446dafc3dc45fd624fb9fe5
JonnyOThan <jonnyothan@gmail.com>
2023-04-10 11:23:20 -0400

    First step in moving PhysicalProp system to FreeIva

--- KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_PhysicalProp.cs
+++ KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_PhysicalProp.cs
@@ -10,212 +10,22 @@ using Valve.VR;
 
 namespace KerbalVR.InternalModules
 {
-	public class VRPhysicalProp : InternalModule
+	public class VRPhysicalProp : FreeIva.PhysicalProp
 	{
-		[KSPField]
-		public bool isSticky = false; // when released, if this is overlapping another collider then it will attach to it
-
-		[KSPField]
-		public string placeSound = string.Empty;
-
-		[KSPField]
-		public string transformName = string.Empty;
-
 		[SerializeField]
 		InteractableBehaviour m_interactableBehaviour;
-		[SerializeField]
-		CollisionTracker m_collisionTracker;
 
-		[SerializeField]
-		Interaction m_interaction;
-
-		Rigidbody m_rigidBody;
-		FreeIva.InternalModuleFreeIva m_freeIvaModule;
-
-		[SerializeField]
-		AudioSource m_audioSource;
-		[SerializeReference]
-		AudioClip m_grabAudioClip;
-		[SerializeReference]
-		AudioClip m_stickAudioClip;
-		[SerializeReference]
-		AudioClip m_impactAudioClip;
-
-		[SerializeField]
-		Collider m_collider;
 		bool m_otherHandGrabbed = false;
-
-		bool m_applyGravity = false;
 
 		public override void OnLoad(ConfigNode node)
 		{
 			base.OnLoad(node);
 
-			if (HighLogic.LoadedScene == GameScenes.LOADING)
+			if (m_collider != null)
 			{
-				var colliderNode = node.GetNode("COLLIDER");
-				if (colliderNode != null)
-				{
-					object obj = new ColliderParams();
-					ConfigNode.LoadObjectFromConfig(obj, colliderNode);
-					var colliderParams = (ColliderParams)obj;
-
-					m_collider = colliderParams.Create(internalProp.hasModel ? transform : internalModel.transform, colliderNode);
-				}
-				else if (transformName != string.Empty)
-				{
-					var colliderTransform = this.FindTransform(transformName);
-					if (colliderTransform != null)
-					{
-						m_collider = colliderTransform.GetComponent<Collider>();
-					}
-				}
-				else
-				{
-					m_collider = GetComponentInChildren<Collider>();
-				}
-
-				if (m_collider != null)
-				{
-					m_collider.isTrigger = false;
-
-					m_interactableBehaviour = m_collider.gameObject.AddComponent<InteractableBehaviour>();
-					m_interactableBehaviour.AttachHandOnGrab = false;
-					
-					m_collider.gameObject.layer = 16; // needs to be 16 to bounce off shell colliders, at least while moving.  Not sure if we want it interacting with the player.
-
-					m_collisionTracker = m_collider.gameObject.AddComponent<CollisionTracker>();
-					m_collisionTracker.PhysicalProp = this;
-				}
-				else
-				{
-					Utils.LogError($"VRPhysicalProp: prop {internalProp.propName} does not have a collider");
-				}
-
-				// setup audio
-				if (m_collider != null)
-				{
-					m_grabAudioClip = LoadAudioClip(node, "grabSound");
-					m_stickAudioClip = LoadAudioClip(node, "stickSound");
-					m_impactAudioClip = LoadAudioClip(node, "impactSound");
-
-					if (m_grabAudioClip != null || m_stickAudioClip != null || m_impactAudioClip != null)
-					{
-						m_audioSource = m_collider.gameObject.AddComponent<AudioSource>();
-						m_audioSource.volume = GameSettings.SHIP_VOLUME;
-						m_audioSource.minDistance = 2;
-						m_audioSource.maxDistance = 10;
-						m_audioSource.playOnAwake = false;
-						m_audioSource.spatialize = true;
-					}
-				}
-
-				// setup interactions
-				var interactionNode = node.GetNode("INTERACTION");
-				if (interactionNode != null)
-				{
-					CreateInteraction(interactionNode);
-				}
+				m_interactableBehaviour = m_collider.gameObject.AddComponent<InteractableBehaviour>();
+				m_interactableBehaviour.AttachHandOnGrab = false;
 			}
-		}
-
-#region static stuff
-		static Dictionary<string, TypeInfo> x_interactionTypes;
-
-		static VRPhysicalProp()
-		{
-			x_interactionTypes = new Dictionary<string, TypeInfo>();
-			foreach (var assembly in AssemblyLoader.loadedAssemblies)
-			{
-				try
-				{
-					assembly.TypeOperation(type =>
-					{
-						if (type.IsSubclassOf(typeof(Interaction)))
-						{
-							x_interactionTypes.Add(type.Name, type.GetTypeInfo());
-						}
-					});
-				}
-				catch (Exception ex)
-				{
-					Debug.LogException(ex);
-				}
-			}
-		}
-
-		public void CreateInteraction(ConfigNode interactionNode)
-		{
-			var name = interactionNode.GetValue("name");
-
-			if (x_interactionTypes.TryGetValue(name, out var typeInfo))
-			{
-				m_interaction = (Interaction)gameObject.AddComponent(typeInfo.AsType());
-				m_interaction.PhysicalProp = this;
-				m_interaction.OnLoad(interactionNode);
-			}
-			else
-			{
-				Utils.LogError($"PROP {internalProp.propName}: No VRPhysicalProp.Interaction named {name} exists");
-			}
-		}
-#endregion
-
-		internal AudioClip LoadAudioClip(ConfigNode node, string key)
-		{
-			string clipUrl = node.GetValue(key);
-			if (clipUrl == null) return null;
-
-			AudioClip result = GameDatabase.Instance.GetAudioClip(clipUrl);
-
-			if (result == null)
-			{
-				Utils.LogError($"Failed to find audio clip {clipUrl} for prop {internalProp.propName}");
-			}
-
-			return result;
-		}
-
-		public void PlayAudioClip(AudioClip clip, float volume, float pitch)
-		{
-			if (clip == null) return;
-			m_audioSource.PlayOneShot(clip);
-		}
-
-		public void PlayAudioClip(AudioClip clip)
-		{
-			PlayAudioClip(clip, GameSettings.SHIP_VOLUME, 1.0f);
-		}
-
-		public void StartAudioLoop(AudioClip clip)
-		{
-			if (clip == null) return;
-			m_audioSource.clip = clip;
-			m_audioSource.loop = true;
-			m_audioSource.volume = GameSettings.SHIP_VOLUME;
-			m_audioSource.pitch = 1.0f;
-			m_audioSource.Play();
-		}
-
-		public void StopAudioLoop()
-		{
-			m_audioSource.loop = false;
-			m_audioSource.Stop();
-		}
-
-		public void OnImpact(float magnitude)
-		{
-			if (m_interaction != null)
-			{
-				m_interaction.OnImpact(magnitude);
-			}
-
-			// TOD: maybe randomize a bit?
-			// m_audioSource.pitch = UnityEngine.Random.Range(-0.2f, 0.2f);
-			float volume = Mathf.InverseLerp(1.0f, 5.0f, magnitude);
-			if (volume == 0) return;
-
-			PlayAudioClip(m_impactAudioClip, volume, UnityEngine.Random.Range(0.8f, 1.2f));
 		}
 
 		void Start()
@@ -233,67 +43,15 @@ namespace KerbalVR.InternalModules
 			m_otherHandGrabbed = true;
 		}
 
-		GameObject rigidBodyObject => m_collider.gameObject;
-
 		private void OnRelease(Hand hand)
 		{
 			if (m_otherHandGrabbed) return;
 
-			m_freeIvaModule = FreeIva.FreeIva.CurrentInternalModuleFreeIva;
 
-			rigidBodyObject.transform.SetParent(m_freeIvaModule.Centrifuge?.IVARotationRoot ?? m_freeIvaModule.internalModel.transform , true);
+			Vector3 linearVelocity = KerbalVR.InteractionSystem.Instance.transform.TransformVector(hand.handActionPose[hand.handType].lastVelocity);
+			Vector3 angularVelocity = KerbalVR.InteractionSystem.Instance.transform.rotation * hand.handActionPose[hand.handType].lastAngularVelocity;
 
-			if (m_interaction)
-			{
-				m_interaction.OnRelease(hand);
-			}
-
-			// are we sticking to something?
-			if (isSticky && m_collisionTracker.ContactCollider != null)
-			{
-				if (m_rigidBody != null)
-				{
-					Component.Destroy(m_rigidBody);
-					m_rigidBody = null;
-				}
-
-				m_applyGravity = false;
-			}
-			else
-			{
-				if (m_rigidBody == null)
-				{
-					m_rigidBody = rigidBodyObject.AddComponent<Rigidbody>();
-				}
-
-				m_collider.isTrigger = false;
-				m_collider.enabled = true;
-
-				m_rigidBody.isKinematic = true;
-				m_rigidBody.useGravity = false;
-
-
-				m_rigidBody.isKinematic = false;
-				m_rigidBody.WakeUp();
-
-				Vector3 propVelocity = KerbalVR.InteractionSystem.Instance.transform.TransformVector(hand.handActionPose[hand.handType].lastVelocity);
-				m_rigidBody.velocity = propVelocity;
-				m_rigidBody.angularVelocity = KerbalVR.InteractionSystem.Instance.transform.rotation * hand.handActionPose[hand.handType].lastAngularVelocity;
-
-				// total hack? - apply reaction velocity in zero-g
-				if (!FreeIva.KerbalIvaAddon.Instance.buckled && !FreeIva.KerbalIvaAddon.Instance.KerbalIva.UseRelativeMovement())
-				{
-					// TODO: should probably have some idea of how much mass this thing is
-					FreeIva.KerbalIvaAddon.Instance.KerbalIva.KerbalRigidbody.WakeUp();
-					FreeIva.KerbalIvaAddon.Instance.KerbalIva.KerbalRigidbody.velocity += -propVelocity * 0.7f;
-				}
-
-				m_applyGravity = true;
-			}
-
-			m_collider.enabled = true;
-
-			// TODO: switch back to kinematic when it comes to rest (or not? it's fun to kick around)
+			base.Release(linearVelocity, angularVelocity);
 		}
 
 		private void OnGrab(Hand hand)
@@ -301,104 +59,28 @@ namespace KerbalVR.InternalModules
 			m_otherHandGrabbed = false;
 			rigidBodyObject.transform.SetParent(hand.handObject.transform, true);
 
-			// disable the collider so it doesn't push us around - or possibly we can just use Physics.IgnoreCollision
-			if (isSticky)
-			{
-				m_collider.isTrigger = true;
-			}
-			else
-			{
-				m_collider.enabled = false;
-			}
-
-			if (m_rigidBody != null)
-			{
-				m_rigidBody.isKinematic = true;
-				m_applyGravity = false;
-			}
-
-			if (m_interaction)
-			{
-				m_interaction.OnGrab(hand);
-			}
-
-			PlayAudioClip(m_grabAudioClip);
+			base.Grab();
 		}
 
-		void FixedUpdate()
+		protected override void PlayStickyFeedback()
 		{
-			if (m_applyGravity)
+			base.PlayStickyFeedback();
+
+			if (isSticky && m_interactableBehaviour.GrabbedHand)
 			{
-				var accel = FreeIva.KerbalIvaAddon.GetInternalSubjectiveAcceleration(m_freeIvaModule, rigidBodyObject.transform.position);
-				m_rigidBody.AddForce(accel, ForceMode.Acceleration);
+				HapticUtils.Light(m_interactableBehaviour.GrabbedHand.handType);
 			}
 		}
 
-		internal class CollisionTracker : MonoBehaviour
+		public interface IVRInteraction
 		{
-			public VRPhysicalProp PhysicalProp;
-			public Collider ContactCollider;
-
-			void FixedUpdate()
-			{
-				ContactCollider = null;
-				enabled = false;
-			}
-
-			void OnCollisionEnter(Collision other)
-			{
-				PhysicalProp.OnImpact(other.relativeVelocity.magnitude);
-			}
-
-			void OnTriggerExit(Collider other)
-			{
-				if (other == ContactCollider)
-				{
-					PhysicalProp.PlayStickyFeedback();
-				}
-			}
-
-			void OnTriggerEnter(Collider other)
-			{
-				if (other.gameObject.layer == 16 && !other.isTrigger && PhysicalProp.m_interactableBehaviour.IsGrabbed && !ColliderIsKerbalIva(other))
-				{
-					PhysicalProp.PlayStickyFeedback();
-					ContactCollider = other;
-					enabled = true;
-				}
-			}
-
-			void OnTriggerStay(Collider other)
-			{
-				// how do we determine if this is a part of the iva shell?
-				if (other.gameObject.layer == 16 && !other.isTrigger && PhysicalProp.m_interactableBehaviour.IsGrabbed && !ColliderIsKerbalIva(other))
-				{
-					ContactCollider = other;
-					enabled = true;
-				}
-			}
-
-			static bool ColliderIsKerbalIva(Collider collider)
-			{
-				return collider.attachedRigidbody == FreeIva.KerbalIvaAddon.Instance.KerbalIva.KerbalRigidbody;
-			}
-		}
-
-		private void PlayStickyFeedback()
-		{
-			if (isSticky)
-			{
-				if (m_interactableBehaviour.GrabbedHand)
-				{
-					HapticUtils.Light(m_interactableBehaviour.GrabbedHand.handType);
-				}
-				PlayAudioClip(m_stickAudioClip);
-			}
+			void OnGrab(Hand hand);
+			void OnRelease(Hand hand);
 		}
 
 		// TODO: maybe this should be pushed up to the interaction system level, and PhysicalProp is a certain kind of Interaction?
 		// then stuff like flightStick etc are also just interactions?
-		public class Interaction : MonoBehaviour
+		public class VRInteraction : MonoBehaviour, IVRInteraction
 		{
 			public VRPhysicalProp PhysicalProp;
 
@@ -409,8 +91,7 @@ namespace KerbalVR.InternalModules
 			public virtual void OnImpact(float magnitude) { }
 		}
 
-
-		public class VRInteractionExtinguisher : Interaction
+		public class VRInteractionExtinguisher : VRInteraction
 		{
 			[SerializeField] Vector3 thrustVector;
 			[SerializeField] Vector3 thrustPosition;
@@ -445,7 +126,7 @@ namespace KerbalVR.InternalModules
 					if (particlePrefab != null)
 					{
 						var particleObject = GameObject.Instantiate(particlePrefab);
-						
+
 						particleObject.layer = 20;
 						particleObject.transform.SetParent(thrustTransform, false);
 						particleObject.transform.localPosition = thrustPosition;
@@ -453,7 +134,7 @@ namespace KerbalVR.InternalModules
 
 						m_particleSystem = particleObject.GetComponent<ParticleSystem>();
 					}
-				}	
+				}
 			}
 
 			public override void OnGrab(Hand hand)
@@ -502,7 +183,7 @@ namespace KerbalVR.InternalModules
 			}
 		}
 
-		public class VRInteractionCamera : Interaction
+		public class VRInteractionCamera : VRInteraction
 		{
 			[SerializeReference] AudioClip m_shutterSound;
 
@@ -553,7 +234,7 @@ namespace KerbalVR.InternalModules
 			}
 		}
 
-		public class VRInteractionFlashlight : Interaction
+		public class VRInteractionFlashlight : VRInteraction
 		{
 			[SerializeReference] AudioClip m_buttonSound;
 			[SerializeField] Material m_lensMaterial;
@@ -620,7 +301,7 @@ namespace KerbalVR.InternalModules
 			}
 		}
 
-		public class VRInteractionSqueak : Interaction
+		public class VRInteractionSqueak : VRInteraction
 		{
 			[SerializeReference] AudioClip m_squeakSound;
 
@@ -663,7 +344,7 @@ namespace KerbalVR.InternalModules
 			}
 		}
 
-		public class VRInteractionBreakable : Interaction
+		public class VRInteractionBreakable : VRInteraction
 		{
 			[SerializeReference] AudioClip breakSound;
 			[SerializeField] float breakSpeed = 4;
@@ -707,7 +388,7 @@ namespace KerbalVR.InternalModules
 
 						audioSource.PlayOneShot(breakSound);
 					}
-					
+
 					GameObject.Destroy(PhysicalProp.rigidBodyObject);
 				}
 			}
