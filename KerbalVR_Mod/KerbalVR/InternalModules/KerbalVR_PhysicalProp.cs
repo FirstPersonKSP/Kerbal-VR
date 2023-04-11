c6c4c8fd3512a662a4261c1dda714613487c4e07
JonnyOThan <jonnyothan@gmail.com>
2023-04-11 12:21:08 -0400

    move the rest of the physical prop interactions and configs to freeiva

--- KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_PhysicalProp.cs
+++ KerbalVR_Mod/KerbalVR/InternalModules/KerbalVR_PhysicalProp.cs
@@ -12,8 +12,13 @@ namespace KerbalVR.InternalModules
 {
 	public class VRPhysicalProp : FreeIva.PhysicalProp
 	{
+		[KSPField]
+		public string vrInteraction = string.Empty;
+
 		[SerializeField]
 		InteractableBehaviour m_interactableBehaviour;
+
+		SteamVR_Action_Boolean_Source m_action;
 
 		bool m_otherHandGrabbed = false;
 
@@ -28,7 +33,7 @@ namespace KerbalVR.InternalModules
 			}
 		}
 
-		void Start()
+		new void Start()
 		{
 			base.Start();
 
@@ -40,15 +45,34 @@ namespace KerbalVR.InternalModules
 			}
 		}
 
+		void BindPinchAction(Hand hand)
+		{
+			if (vrInteraction != string.Empty)
+			{
+				m_action = SteamVR_Input.GetBooleanAction("default", vrInteraction)[hand.handType];
+				m_action.onStateDown += OnPinchStateDown;
+				m_action.onStateUp += OnPinchStateUp;
+			}
+		}
+
+		void UnbindPinchAction()
+		{
+			m_action.onStateDown -= OnPinchStateDown;
+			m_action.onStateUp -= OnPinchStateUp;
+		}
+
 		private void OnOtherHandGrab(Hand hand)
 		{
+			UnbindPinchAction();
+			BindPinchAction(hand);
 			m_otherHandGrabbed = true;
 		}
 
 		private void OnRelease(Hand hand)
 		{
-			if (m_otherHandGrabbed) return;
+			UnbindPinchAction();
 
+			if (m_otherHandGrabbed) return;
 
 			Vector3 linearVelocity = KerbalVR.InteractionSystem.Instance.transform.TransformVector(hand.handActionPose[hand.handType].lastVelocity);
 			Vector3 angularVelocity = KerbalVR.InteractionSystem.Instance.transform.rotation * hand.handActionPose[hand.handType].lastAngularVelocity;
@@ -62,6 +86,18 @@ namespace KerbalVR.InternalModules
 			rigidBodyObject.transform.SetParent(hand.handObject.transform, true);
 
 			base.Grab();
+
+			BindPinchAction(hand);
+		}
+
+		private void OnPinchStateDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+		{
+			StartInteraction();
+		}
+
+		private void OnPinchStateUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+		{
+			StopInteraction();
 		}
 
 		protected override void PlayStickyFeedback()
@@ -71,275 +107,6 @@ namespace KerbalVR.InternalModules
 			if (isSticky && m_interactableBehaviour.GrabbedHand)
 			{
 				HapticUtils.Light(m_interactableBehaviour.GrabbedHand.handType);
-			}
-		}
-
-		public interface IVRInteraction
-		{
-			void OnGrab(Hand hand);
-			void OnRelease(Hand hand);
-		}
-
-		// TODO: maybe this should be pushed up to the interaction system level, and PhysicalProp is a certain kind of Interaction?
-		// then stuff like flightStick etc are also just interactions?
-		public class VRInteraction : FreeIva.PhysicalProp.Interaction, IVRInteraction
-		{
-			public new VRPhysicalProp VRPhysicalProp => PhysicalProp as VRPhysicalProp;
-
-			public virtual void OnGrab(Hand hand) { base.OnGrab(); }
-			public virtual void OnRelease(Hand hand) { base.OnRelease(); }
-		}
-
-		public class VRInteractionExtinguisher : VRInteraction
-		{
-			[SerializeField] Vector3 thrustVector;
-			[SerializeField] Vector3 thrustPosition;
-			public Transform thrustTransform;
-			public AudioClip m_audioClip;
-			[SerializeField] ParticleSystem m_particleSystem;
-
-			SteamVR_Action_Boolean_Source m_pinchAction;
-			bool m_playingSound;
-
-			public override void OnLoad(ConfigNode interactionNode)
-			{
-				base.OnLoad(interactionNode);
-
-				string transformName = interactionNode.GetValue("thrustTransformName");
-				thrustTransform = PhysicalProp.FindTransform(transformName);
-				enabled = false;
-
-				interactionNode.TryGetValue(nameof(thrustPosition), ref thrustPosition);
-
-				if (!interactionNode.TryGetValue(nameof(thrustVector), ref thrustVector))
-				{
-					Utils.LogError($"PROP {PhysicalProp.internalProp.propName} VRInteractionExtinguisher could not parse key {nameof(thrustVector)}");
-				}
-
-				m_audioClip = PhysicalProp.LoadAudioClip(interactionNode, "sound");
-
-				string particleSystemName = string.Empty;
-				if (interactionNode.TryGetValue(nameof(particleSystemName), ref particleSystemName))
-				{
-					var particlePrefab = AssetLoader.Instance.GetGameObject(particleSystemName);
-					if (particlePrefab != null)
-					{
-						var particleObject = GameObject.Instantiate(particlePrefab);
-
-						particleObject.layer = 20;
-						particleObject.transform.SetParent(thrustTransform, false);
-						particleObject.transform.localPosition = thrustPosition;
-						particleObject.transform.localRotation = Quaternion.FromToRotation(Vector3.forward, -thrustVector);
-
-						m_particleSystem = particleObject.GetComponent<ParticleSystem>();
-					}
-				}
-			}
-
-			public override void OnGrab(Hand hand)
-			{
-				base.OnGrab(hand);
-
-				// TODO: pinchIndex is a boolean action - should it be a float?
-				m_pinchAction = SteamVR_Input.GetBooleanAction("default", "PinchIndex")[hand.handType];
-				enabled = true;
-				m_particleSystem.gameObject.SetActive(true);
-			}
-
-			public override void OnRelease(Hand hand)
-			{
-				base.OnRelease(hand);
-
-				m_pinchAction = null;
-				enabled = false;
-			}
-
-			void FixedUpdate()
-			{
-				if (m_pinchAction.state)
-				{
-					Vector3 accelerationVector = thrustTransform.TransformVector(thrustVector);
-
-					FreeIva.KerbalIvaAddon.Instance.KerbalIva.KerbalRigidbody.WakeUp();
-					FreeIva.KerbalIvaAddon.Instance.KerbalIva.KerbalRigidbody.AddForceAtPosition(accelerationVector, thrustTransform.position, ForceMode.Acceleration);
-
-					if (!m_playingSound)
-					{
-						PhysicalProp.StartAudioLoop(m_audioClip);
-						m_playingSound = true;
-						m_particleSystem.Play();
-					}
-				}
-				else
-				{
-					if (m_playingSound)
-					{
-						PhysicalProp.StopAudioLoop();
-						m_playingSound = false;
-						m_particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmitting);
-					}
-				}
-			}
-		}
-
-		public class VRInteractionCamera : VRInteraction
-		{
-			[SerializeReference] AudioClip m_shutterSound;
-
-			SteamVR_Action_Boolean_Source m_pinchAction;
-
-			public override void OnLoad(ConfigNode interactionNode)
-			{
-				base.OnLoad(interactionNode);
-
-				m_shutterSound = PhysicalProp.LoadAudioClip(interactionNode, "shutterSound");
-			}
-
-			public override void OnGrab(Hand hand)
-			{
-				base.OnGrab(hand);
-
-				m_pinchAction = SteamVR_Input.GetBooleanAction("default", "PinchIndex")[hand.handType];
-				m_pinchAction.onStateDown += OnPinchStateDown;
-				enabled = true;
-			}
-
-			public override void OnRelease(Hand hand)
-			{
-				base.OnRelease(hand);
-
-				m_pinchAction.onStateDown -= OnPinchStateDown;
-			}
-
-			void OnDestroy()
-			{
-				if (m_pinchAction != null)
-				{
-					m_pinchAction.onStateDown -= OnPinchStateDown;
-				}
-			}
-
-			private void OnPinchStateDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-			{
-				PhysicalProp.PlayAudioClip(m_shutterSound);
-
-				string screenshotDir = Application.platform == RuntimePlatform.OSXPlayer
-					? Path.Combine(Application.dataPath, "../../Screenshots")
-					: Path.Combine(Application.dataPath, "../Screenshots");
-				string screenshotFileName = Path.GetRandomFileName();
-				string screenshotPath = Path.ChangeExtension(Path.Combine(screenshotDir, screenshotFileName), ".png");
-
-				ScreenCapture.CaptureScreenshot(screenshotPath, ScreenCapture.StereoScreenCaptureMode.BothEyes);
-			}
-		}
-
-		public class VRInteractionFlashlight : VRInteraction
-		{
-			[SerializeReference] AudioClip m_buttonSound;
-			[SerializeField] Material m_lensMaterial;
-			[SerializeField] Light m_light;
-
-			SteamVR_Action_Boolean_Source m_pinchThumbAction;
-
-			static int EMISSIVE_COLOR_PROPERTY_ID = Shader.PropertyToID("_EmissiveColor");
-
-			public override void OnLoad(ConfigNode interactionNode)
-			{
-				base.OnLoad(interactionNode);
-
-				m_buttonSound = PhysicalProp.LoadAudioClip(interactionNode, "buttonSound");
-				m_lensMaterial = PhysicalProp.FindTransform(interactionNode.GetValue("lensObjectName"))?.GetComponent<MeshRenderer>()?.material;
-				m_light = PhysicalProp.FindTransform(interactionNode.GetValue("lightObjectName"))?.GetComponent<Light>();
-
-				if (m_light != null)
-				{
-					m_light.enabled = false;
-				}
-
-				if (m_lensMaterial != null)
-				{
-					m_lensMaterial.SetColor(EMISSIVE_COLOR_PROPERTY_ID, Color.black);
-				}
-			}
-
-			public override void OnGrab(Hand hand)
-			{
-				base.OnGrab(hand);
-
-				m_pinchThumbAction = SteamVR_Input.GetBooleanAction("default", "PinchThumb")[hand.handType];
-				m_pinchThumbAction.onStateDown += OnButtonStateDown;
-			}
-
-			public override void OnRelease(Hand hand)
-			{
-				base.OnRelease(hand);
-				m_pinchThumbAction.onStateDown -= OnButtonStateDown;
-			}
-
-			void OnDestroy()
-			{
-				if (m_pinchThumbAction != null)
-				{
-					m_pinchThumbAction.onStateDown -= OnButtonStateDown;
-				}
-			}
-
-			private void OnButtonStateDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-			{
-				PhysicalProp.PlayAudioClip(m_buttonSound);
-
-				if (m_light != null)
-				{
-					m_light.enabled = !m_light.enabled;
-
-					if (m_lensMaterial != null)
-					{
-						m_lensMaterial.SetColor(EMISSIVE_COLOR_PROPERTY_ID, m_light.enabled ? Color.white : Color.black);
-					}
-				}
-			}
-		}
-
-		public class VRInteractionSqueak : VRInteraction
-		{
-			[SerializeReference] AudioClip m_squeakSound;
-
-			SteamVR_Action_Boolean_Source m_pinchAction;
-
-			public override void OnLoad(ConfigNode interactionNode)
-			{
-				base.OnLoad(interactionNode);
-				m_squeakSound = PhysicalProp.LoadAudioClip(interactionNode, "squeakSound");
-			}
-
-			public override void OnGrab(Hand hand)
-			{
-				base.OnGrab(hand);
-				m_pinchAction = SteamVR_Input.GetBooleanAction("default", "PinchIndex")[hand.handType];
-				m_pinchAction.onStateDown += OnPinchStateDown;
-				enabled = true;
-
-				PhysicalProp.PlayAudioClip(m_squeakSound);
-			}
-
-			public override void OnRelease(Hand hand)
-			{
-				base.OnRelease(hand);
-
-				m_pinchAction.onStateDown -= OnPinchStateDown;
-			}
-
-			void OnDestroy()
-			{
-				if (m_pinchAction != null)
-				{
-					m_pinchAction.onStateDown -= OnPinchStateDown;
-				}
-			}
-
-			private void OnPinchStateDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-			{
-				PhysicalProp.PlayAudioClip(m_squeakSound);
 			}
 		}
 	}
