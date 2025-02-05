2b2f221d8bbee515f6810e20def411d76ed1ec57
JonnyOThan <jonnyothan@gmail.com>
2025-02-05 16:31:24 -0500

    switching to sdk style projects

--- KerbalVR_Mod/KerbalVR-MAS/Properties/AssemblyInfo.cs
+++ KerbalVR_Mod/KerbalVR-MAS/Properties/AssemblyInfo.cs
@@ -5,11 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KerbalVR-MAS")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KerbalVR-MAS")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -22,20 +18,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d2efbfe8-da01-427f-abfa-f2f52278787d")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.4.0")]
-[assembly: AssemblyFileVersion("0.9.4.0")]
-
-
-[assembly: KSPAssembly("KerbalVR-MAS", 0, 9, 4)]
-
-[assembly: KSPAssemblyDependency("MOARdVAvionicsSystems", 1, 3)] 
--- KerbalVR_Mod/KerbalVR-RPM/Properties/AssemblyInfo.cs
+++ KerbalVR_Mod/KerbalVR-RPM/Properties/AssemblyInfo.cs
@@ -5,11 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KerbalVR-RPM")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KerbalVR-RPM")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,20 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("132c7d00-6367-4312-bae1-4286435638e0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.4.0")]
-[assembly: AssemblyFileVersion("0.9.4.0")]
-
-[assembly: KSPAssembly("KerbalVR-RPM", 0, 9, 4)] 
-
-[assembly: KSPAssemblyDependency("RasterPropMonitor", 0, 18)]
